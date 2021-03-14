6f94e8641d1ffcb5284ec3551bb294fbf23033cc
Ren510 <yt.1141977@gmail.com>
2021-03-14 12:46:59 +0000

    [Start]5-5

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,13 +5,15 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image)  
+    redirec
+    t_to post_image_
+    path(post_image)  
     
     end
-
-  def destroy
+    
+　def destroy
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+    redirect_to post_image_path(params[:post_image_id])  
   end
 
   private
